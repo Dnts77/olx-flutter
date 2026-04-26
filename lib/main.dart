c4a09ff9bccc33328f4677310fd823c50ff40bda
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-04-26 12:49:13 -0300

    :tada: init: Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:olx_flutter/screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+    title: "OLX",
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
