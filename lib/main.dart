76819cec02e468f2da53b166a68b176c496de654
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-04-27 19:58:39 -0300

    :art: feat: Uso da classe de temas no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:olx_flutter/screens/home_screen.dart';
+import 'package:olx_flutter/utils/themes.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomeScreen(),
     title: "OLX",
+    theme: AppTheme.defaultTheme,
     debugShowCheckedModeBanner: false,
   ));
 }
