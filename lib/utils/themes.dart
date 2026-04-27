dc5379932872d39288615b81a331868f4493efd4
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-04-27 19:56:49 -0300

    :lipstick: style: Classe para temas

---
+++ lib/utils/themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static ThemeData defaultTheme = ThemeData(
+    primaryColor: Color(0xff9c27b0),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Color(0xff9c27b0),
+      shadowColor: Color(0xff7b1fa2),
+      elevation: 4,
+    )
+  );
+}
