4529f6f36d4bfd889c15633f2baa4630d6ef50e6
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-04-28 19:21:39 -0300

    :sparkles: feat: Lógica de login e cadastro

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:olx_flutter/models/user_model.dart';
+import 'package:olx_flutter/widgets/customized_text_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,7 +12,59 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   bool _signUp = false;
+  String _errorMessage = "";
+  String _buttonText = "Entrar";
+
+  //Cadastrar
+  void _signUpUser(UserModel user){
+    FirebaseAuth auth = FirebaseAuth.instance;
+    auth.createUserWithEmailAndPassword(
+      email: user.email,
+      password: user.senha
+    ).then((firebaseUser){
+
+    });
+  }
+
+  //Logar
+  void _logInUser(UserModel user){
+    FirebaseAuth auth = FirebaseAuth.instance;
+    auth.signInWithEmailAndPassword(
+      email: user.email,
+      password: user.senha
+    ).then((firebaseUser){
+
+    });
+  }
+
+  //Validando os campos
+  void _validateFields(){
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    if(email.isEmpty || password.isEmpty){
+      setState(() {
+        _errorMessage = "Preencha todos os campos!";
+      });
+    }
+    else if(email.isNotEmpty && email.contains("@")){
+      if(password.isNotEmpty && password.length > 6){
+
+        UserModel user = UserModel();
+        user.email = email;
+        user.senha = password;
+
+        if(_signUp){
+          _signUpUser(user);
+        }else{
+          _logInUser(user);
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,37 +87,16 @@ class _HomeScreenState extends State<HomeScreen> {
                     height: 150,
                   ),
                 ),
-                TextField(
-                  //controller: ,
+                CustomizedTextField(
+                  controller: _emailController,
+                  hint: "Email",
                   autofocus: true,
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(
-                    fontSize: 20
-                  ),
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                    hintText: "Email",
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6)
-                    )
-                  ),
+                  type: TextInputType.emailAddress,
                 ),
-                TextField(
-                  //controller: ,
-                  obscureText: true,
-                  keyboardType: TextInputType.text,
-                  style: TextStyle(
-                    fontSize: 20
-                  ),
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                    hintText: "Senha",
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6)
-                    )
-                  ),
+                CustomizedTextField(
+                  controller: _passwordController,
+                  hint: "Senha",
+                  type: TextInputType.text,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -73,6 +107,10 @@ class _HomeScreenState extends State<HomeScreen> {
                       onChanged: (bool value){
                         setState(() {
                           _signUp = value;
+                          _buttonText = "Entrar";
+                          if(_signUp){
+                            _buttonText = "Cadastrar";
+                          }
                         });
                       },
                     ),
@@ -88,12 +126,23 @@ class _HomeScreenState extends State<HomeScreen> {
                     )
                   ),
                   onPressed: (){
-
+                    _validateFields();
                   },
                   child: Text(
-                    "Entrar",
+                    _buttonText,
                     style: TextStyle(
                       fontSize: 20
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(top: 20),
+                  child: Text(
+                    _errorMessage,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold
                     ),
                   ),
                 )
