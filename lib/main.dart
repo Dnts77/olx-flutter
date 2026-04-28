70581e80d98262be54230415acf55d568f6e7b57
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-04-28 19:20:17 -0300

    :sparkles: feat: Inicialização do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:olx_flutter/firebase_options.dart';
 import 'package:olx_flutter/screens/home_screen.dart';
 import 'package:olx_flutter/utils/themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(MaterialApp(
     home: HomeScreen(),
     title: "OLX",
