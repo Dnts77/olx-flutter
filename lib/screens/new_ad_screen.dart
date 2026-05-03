15635652e25fa4e23026a8f43c3001a99be48a24
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-05-03 19:31:52 -0300

    :sparkles: feat: Começo da lógica para adicionar novos anúncios

--- lib/screens/new_ad_screen.dart
+++ lib/screens/new_ad_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:olx_flutter/widgets/customized_button.dart';
 
@@ -12,6 +14,11 @@ class NewAdScreen extends StatefulWidget {
 class _NewAdScreenState extends State<NewAdScreen> {
 
   final _formKey = GlobalKey<FormState>();
+  final List<File> _imagesList = [];
+
+  void _selectGalleryImage(){
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +35,70 @@ class _NewAdScreenState extends State<NewAdScreen> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 //Imagens
-                /*FormField(
-                  builder: (field) {
-                    throw '';
+                FormField<List>(
+                  initialValue: _imagesList,
+                  validator: (images){
+                    if(images!.isEmpty){
+                      return "Necessário selecionar uma imagem!";
+                    }
+                    return null;
                   },
-                ),*/
+                  builder: (field) {
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: 100,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: _imagesList.length + 1, //0
+                            itemBuilder: (context, index){
+                              if(index == _imagesList.length){
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 8),
+                                  child: GestureDetector(
+                                    onTap: (){
+                                      _selectGalleryImage();
+                                    },
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.grey[400],
+                                      radius: 50,
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Icon(
+                                            Icons.add_a_photo,
+                                            size: 40,
+                                            color: Colors.grey[100],
+                                          ),
+                                          Text(
+                                            "Adicionar",
+                                            style: TextStyle(color: Colors.grey[100]),
+                                          )
+                                        ],
+                                      ), 
+                                    ),
+                                  ),
+                                );
+                              }
+                              if(_imagesList.isNotEmpty){
+
+                              }
+                              return Container();
+                            },
+                          ),
+                        ),
+                        if(field.hasError)
+                          Container(
+                            padding: EdgeInsets.all(2),
+                            child: Text(
+                              "${field.errorText}",
+                              style: TextStyle(color: Colors.red, fontSize: 14),
+                            ),
+                          )
+                      ],
+                    );
+                  },
+                ),
 
                 //Dropdown
                 Row(
