bf2789f22f63568c7a671a2b9cf06782bf4be5d8
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-05-06 18:19:22 -0300

    :sparkles: feat: Novos parâmetros para o TextField

--- lib/widgets/customized_text_field.dart
+++ lib/widgets/customized_text_field.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 
 class CustomizedTextField extends StatelessWidget {
-  const CustomizedTextField({super.key, required this.controller, required this.hint, this.obscure = false, this.autofocus = false, this.type = TextInputType.text});
+  const CustomizedTextField({super.key, required this.controller, required this.hint, this.obscure = false, this.autofocus = false, this.type = TextInputType.text, this.inputFormatters, this.maxLines, this.validator});
 
 
   final TextEditingController controller;
@@ -9,16 +11,22 @@ class CustomizedTextField extends StatelessWidget {
   final bool obscure;
   final bool autofocus;
   final TextInputType type;
+  final int? maxLines;
+  final List<TextInputFormatter>? inputFormatters;
+  final FormFieldValidator<String>? validator;
 
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: controller,
       autofocus: autofocus,
       obscureText: obscure,
       keyboardType: type,
+      inputFormatters: inputFormatters,
+      validator: validator,
       style: TextStyle(fontSize: 20),
+      maxLines: maxLines,
       decoration: InputDecoration(
         contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
         hintText: hint,
